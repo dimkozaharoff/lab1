001c484301f37c1f18f72ff1e3f249a9579ac52b
zdv <devilion@mail.ru>
2026-06-03 04:34:01 +0300

    Добавлен ввод двух чисел

--- main.cpp
+++ main.cpp
@@ -1,2 +1,11 @@
 #include "sdt.h"
-int main() { }
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Введите два целых числа: ";
+    cin >> a >> b;
+    return 0;
+}
